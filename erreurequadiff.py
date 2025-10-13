08b0c0d435515fb41cb5d117232c773fa50e2554
Myrina Lafage <lfm4857a@u6-100-20d.univ-tlse3.fr>
2025-10-13 14:09:32 +0200

    tracé des courbes ok

---
+++ erreurequadiff.py
@@ -0,0 +1,49 @@
+#Methodes de resolution d'equations différentielles
+import matplotlib.pyplot as plt
+import numpy as np
+
+methodes=np.loadtxt("equadiff.txt")
+erreurs=np.loadtxt("erreurequadiff.txt")
+
+plt.figure()
+plt.plot(methodes[:,0],methodes[:,1],label="Euler")
+plt.plot(methodes[:,0],methodes[:,2],label="Heun")
+plt.plot(methodes[:,0],methodes[:,3],label="RK4")
+plt.plot(methodes[:,0],methodes[:,4],label="Theorie exp(-x)")
+plt.grid()
+plt.legend()
+plt.xlabel("x")
+plt.ylabel("y(x)")
+plt.title("Comparaison des methodes de resolution a la theorie")
+
+h=erreurs[:,0]
+
+plt.figure()
+plt.plot(erreurs[:,0],erreurs[:,1],label="Euler")
+plt.grid()
+plt.legend()
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel("h (pas)")
+plt.ylabel("y(x)")
+plt.title("Erreurs des methodes de resolution d'equadiff en fonction du pas")
+
+plt.figure()
+plt.plot(erreurs[:,0],erreurs[:,2],label="Heun")
+plt.grid()
+plt.legend()
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel("h (pas)")
+plt.ylabel("y(x)")
+plt.title("Erreurs des methodes de resolution d'equadiff en fonction du pas")
+
+plt.figure()
+plt.plot(erreurs[:,0],erreurs[:,3],label="RK4")
+plt.grid()
+plt.legend()
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel("h (pas)")
+plt.ylabel("y(x)")
+plt.title("Erreurs des methodes de resolution d'equadiff en fonction du pas")
